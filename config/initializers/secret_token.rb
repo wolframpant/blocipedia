07612bf9ab01c5a89b824a4d2ec6aa5ff26968e7
Ellen W <llnwlfsn@gmail.com>
2014-11-08 02:01:03 +0000

    Updated Gemfile for deployment to Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blocipedia::Application.config.secret_key_base = '10a91dcb2f12886722b56096e7008c19d3abb58cb402b8a10ced1d59ec2192cc633ab29a577bae8366219f5ff267287823a6caef047017daa5a9b87e36c08ee2'
